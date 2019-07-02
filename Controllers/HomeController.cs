1e7a6394a80eea2e4aec9b6d3b63b12cafedfdbb
Alvarden15 <alvaroracchumi@hotmail.com>
2019-07-02 11:38:12 -0500

    Prueba de conexion de API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,14 +5,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PruebaTallerProyecto.Models;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
 
 namespace PruebaTallerProyecto.Controllers
 {
     public class HomeController : Controller
     {
+        //Como se llama a un metodo GET de una web API
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<String> lista=null;
+            
+            //Se utiliza un HttpClient
+            using(var cliente= new HttpClient()){
+                //Se llama a la URL de la API
+                cliente.BaseAddress= new Uri("12.31.2.3/api/");
+                //Se invoca el metodo Get, especificando de que controlador
+                var task=cliente.GetAsync("values");
+                //Se obtiene el resultado
+                var resultado=task.Result;
+                //Valida si tuvo exito o no la extracción
+                if(resultado.IsSuccessStatusCode){
+                    //Se lee el resultado como se indica
+                    var read=resultado.Content.ReadAsAsync<List<String>>();
+                    lista=read.Result;
+                }else{
+                    //Si ocurrió un error, llama a un mensaje de error
+                ModelState.AddModelError(String.Empty,"No se pudo establecer contacto con el servidor. Por favor intente de nuevo");
+                }
+            }
+            
+            return View(lista);
         }
 
         public IActionResult Privacy()
